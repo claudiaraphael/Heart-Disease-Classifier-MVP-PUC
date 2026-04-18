8d84d050c90f4cfcf5e67346a22596e97ced70f0
claudia <50813092+claudiaraphael@users.noreply.github.com>
2026-04-18 16:42:49 -0300

    correction, created __init__.py for /model/

--- api/model/__init__.py
+++ api/model/__init__.py
@@ -3,33 +3,30 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
 
-# importando os elementos definidos no modelo
 from model.base import Base
 from model.paciente import Paciente
-from model.modelo import Model
 from model.pipeline import Pipeline
 from model.preprocessador import PreProcessador
 from model.avaliador import Avaliador
 from model.carregador import Carregador
 
+# Garante que o diretório do banco de dados existe
 db_path = "database/"
-# Verifica se o diretorio não existe
 if not os.path.exists(db_path):
-   # então cria o diretorio
-   os.makedirs(db_path)
+    os.makedirs(db_path)
 
-# url de acesso ao banco (essa é uma url de acesso ao sqlite local)
-db_url = 'sqlite:///%s/pacientes.sqlite3' % db_path
+# URL de acesso ao banco SQLite local
+db_url = "sqlite:///%s/pacientes.sqlite3" % db_path
 
-# cria a engine de conexão com o banco
+# Cria a engine de conexão com o banco
 engine = create_engine(db_url, echo=False)
 
-# Instancia um criador de seção com o banco
+# Instancia um criador de sessão com o banco
 Session = sessionmaker(bind=engine)
 
-# cria o banco se ele não existir 
+# Cria o banco se ele não existir
 if not database_exists(engine.url):
-    create_database(engine.url) 
+    create_database(engine.url)
 
-# cria as tabelas do banco, caso não existam
+# Cria as tabelas do banco, caso não existam
 Base.metadata.create_all(engine)
